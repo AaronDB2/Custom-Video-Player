d82ffac60adc336d514954a8b740d01d8fb47c7e
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-08 17:11:45 +0100

    Added playback speed

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
+const speed = document.querySelector('.play-speed');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 
@@ -107,7 +108,9 @@ function toggleMute() {
 
 // Change Playback Speed -------------------- //
 
-
+function changeSpeed() {
+	video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
 
@@ -119,3 +122,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
