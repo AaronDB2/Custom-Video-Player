e37a172bd94db2a09473451f7a191e3cc3ae4911
Aaron de Boer <aarondeboerwk@gmail.com>
2022-11-08 16:25:08 +0100

    Porgress bar is now clickable and made the progress numbers work

--- script.js
+++ script.js
@@ -33,9 +33,26 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+	const minutes = Math.floor(time / 60);
+	let seconds = Math.floor(time % 60);
+	seconds = seconds > 9 ? seconds : `0${seconds}`;
+	return `${minutes}:${seconds}`;
+} 
+
 // Update progress bar as video plays
 function updateProgress() {
 	progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+	currentTime.textContent = `${displayTime(video.currentTime)} /`;
+	duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e){
+	const newTime = e.offsetX / progressRange.offsetWidth;
+	progressBar.style.width = `${newTime * 100}%`;
+	video.currentTime = newTime * video.duration;
 }
 
 // Volume Controls --------------------------- //
@@ -53,3 +70,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
